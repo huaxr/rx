6336afc84038cd37dd975217ac7ec81320368a3a
huaxinrui <huaxinrui@bytedance.com>
2021-02-05 00:43:05 +0800

    feat: sub group

--- ctx/group.go
+++ ctx/group.go
@@ -8,15 +8,22 @@ import (
 	"strings"
 )
 
+type GroupI interface {
+	Register(method, path string, handlerFuncs ...handlerFunc)
+	Group(path string, handlerFuncs ...handlerFunc) GroupI
+}
+
 type g struct {
 	path     string
 	handlers []handlerFunc
+	pre, next 	*g
 }
 
-func Group(path string, handlerFuncs ...handlerFunc) *g {
+func Group(path string, handlerFuncs ...handlerFunc) GroupI {
 	g := new(g)
 	g.handlers = handlerFuncs
-
+	g.pre = nil
+	g.next = nil
 	if !strings.HasPrefix(path, "/") {
 		path = "/" + path
 	}
@@ -28,11 +35,30 @@ func Group(path string, handlerFuncs ...handlerFunc) *g {
 	return g
 }
 
+func (gp *g) Group(path string, handlerFuncs ...handlerFunc) GroupI{
+	g := new(g)
+	g.handlers = handlerFuncs
+	g.pre = gp
+	g.next = nil
+	return g
+}
+
+
 func (g *g) Register(method, path string, handlerFuncs ...handlerFunc) {
+	// when the previous g exist. which means the router has it's
+	// group parent may contains handlers to be execute first.
+	var preHandlers []handlerFunc
+	var prePath string
+	for g.pre != nil {
+		p := g.pre
+		prePath += p.path
+		preHandlers = append(preHandlers, p.handlers...)
+		p = p.pre
+	}
 	if !strings.HasPrefix(path, "/") {
 		path = "/" + path
 	}
-	url := g.path + path
+	url := prePath + g.path + path
 	handlers := []handlerFunc{}
 	handlers = append(handlers, g.handlers...)
 	handlers = append(handlers, handlerFuncs...)
--- example/example.go
+++ example/example.go
@@ -2,7 +2,42 @@ package main
 
 import (
 	"github.com/huaxr/rx/ctx"
+	"log"
+	"time"
 )
+
+func handler(ctx ctx.ReqCxtI) {
+	log.Println("execute handler1")
+}
+
+func handler2(ctx ctx.ReqCxtI) {
+	log.Println("execute handler2")
+}
+
+func handler3(ctx ctx.ReqCxtI) {
+	log.Println("execute handler3")
+	ctx.Set("a", []string{"a", "b", "c"})
+	ctx.Next(handler4)
+	ctx.JSON(200, map[string]interface{}{"time": time.Now(), "ctx": "xxxxxx"})
+}
+
+func handler4(ctx ctx.ReqCxtI) {
+	log.Println("execute handler4")
+}
+
+type PostBody struct {
+	Name string `json:"name"`
+}
+
+func handler5(ctx ctx.ReqCxtI) {
+	log.Println(string(ctx.GetBody()))
+	var post PostBody
+	err := ctx.ParseBody(&post)
+	ctx.JSON(200, map[string]interface{}{"time": time.Now(), "ctx": post.Name})
+	log.Println("execute handler5", err, post.Name)
+	ctx.Next(handler)
+	//ctx.Abort(200, "sorry")
+}
 
 // std epoll
 func main() {
@@ -13,9 +48,15 @@ func main() {
 		ctx.Abort(404, "not exist")
 	})
 
-	group := ctx.Group("/api/auth/", handler4, handler2)
+	group := ctx.Group("/api/auth", handler4, handler2)
 	group.Register("get", "/user", handler)
-	group.Register("get", "user2", handler)
+	group.Register("get", "/user2", handler)
+	// 递归组
+	sub := group.Group("/v2", handler)
+	sub.Register("get", "/la", handler)
+	sub2 := sub.Group("/v3", handler)
+	sub2.Register("get", "/la", handler)
+
 
 	ctx.Register("get", "/aaa", handler3, handler2, handler)
 	ctx.Register("post", "/bbb", handler5)
